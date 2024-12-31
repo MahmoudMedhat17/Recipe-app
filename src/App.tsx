3d9c3e0c97fe1fa629d8f312587744f9cfbc745b
Mahmoud Medhat <M7moooud.17@gmail.com>
2024-12-31 05:36:57 +0200

    Made some improvements in design.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
     <div className="xl:px-52">
       <Pages />
       <button onClick={ScrollToTop}>
-        <FaCircleArrowUp className="fixed text-gray-700/80 hover:text-orange-500 duration-200 cursor-pointer bottom-4 right-8 w-10 sm:w-12 md:w-14 h-10 sm:h-12 md:w-14" />{" "}
+        <FaCircleArrowUp className="fixed text-gray-700/80 hover:text-orange-500 duration-200 cursor-pointer bottom-4 right-8 w-10 sm:w-12 md:w-14 h-10 sm:h-12 md:h-14" />{" "}
       </button>
     </div>
   );
