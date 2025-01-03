439c1ee7988cd4f7c07c9e77bc401ea97c059b59
Mahmoud Medhat <M7moooud.17@gmail.com>
2025-01-03 17:01:07 +0200

    Updated the way to use React router dom.

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,48 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import {
+  // BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Cuisines from "./pages/Cuisines.tsx";
+import SearchedCuisine from "./components/SearchedCuisine.tsx";
+import CuisineCard from "./components/CuisineCard.tsx";
+import Home from "./pages/Home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/cuisines/:cuisineType",
+        element: <Cuisines />,
+      },
+      {
+        path: "/searched/:userQuery",
+        element: <SearchedCuisine />,
+      },
+      {
+        path: "/cuisine/:cuisineId",
+        element: <CuisineCard />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* Old way to use React router dom */}
+    {/* <BrowserRouter>
       <App />
-    </BrowserRouter>
+    </BrowserRouter> */}
+    {/* New way to user React router dom */}
+    <RouterProvider router={router} />
   </StrictMode>
 );
--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,21 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route, createBrowserRouter } from "react-router-dom";
 import Navbar from "@/components/Navbar";
-import Home from "./Home";
-import Cuisines from "./Cuisines";
-import SearchedCuisine from "@/components/SearchedCuisine";
+// import Home from "./Home";
+// import Cuisines from "./Cuisines";
+// import SearchedCuisine from "@/components/SearchedCuisine";
 import Footer from "@/components/Footer";
-import CuisineCard from "@/components/CuisineCard";
+// import CuisineCard from "@/components/CuisineCard";
+import { Outlet } from "react-router-dom";
 
 const Pages = () => {
   return (
     <>
       <Navbar />
-      <Routes>
+      <main>
+        {/* Outlet renders the children */}
+        <Outlet />
+      </main>
+      {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cuisines/:cuisineType" element={<Cuisines />} />
         <Route path="/searched/:userQuery" element={<SearchedCuisine />} />
         <Route path="/cuisine/:cuisineId" element={<CuisineCard />} />
-      </Routes>
+      </Routes> */}
       <Footer />
     </>
   );
